8b951646cb9f94a92659c420cdd0897e52c46779
SamHaggans <swhaggans01@bvsd.org>
2019-06-05 22:06:22 -0600

    Comment code and add README

---
+++ README.md
@@ -0,0 +1,12 @@
+# Sea Ice Animation Project
+This site uses OpenLayers as the API to load images from NSIDC's Geoserver of Sea Ice data. 
+The site currently uses an expressjs server to load the page, but otherwise it is just the data in /public.
+
+# Information about loading layers
+Because OpenLayers is not designed to be actively loading layers frequently, which an animation needs, zooming or panning 
+when loading layers would cause the layers to fail to load and have the map turn blank.
+To avoid this, the program pre-loads 30 layers into the map. This means that for the first 30 frames, they are added
+as quickly as possible but not displayed in the map. For later frames, the program displays the frame that is 30 frames ahead of the "current" frame, where the current frame is the one being loaded by the program. This means that OpenLayers does not have to deal with loading the frames at the same time as zooming or panning, which stops the issues with blank
+maps. 
+However, because of this, there are 30 frames left over after all dates have been cycled through. This is why, at the end of the program, the remaining frames are displayed in a while loop until all frames have been shown.
+Still, the dates are also displayed in a similar way to the frames, so the displayed date is always displayed with the correct frame, not 30 layers off.
--- public/script.js
+++ public/script.js
@@ -1,176 +1,129 @@
-var stop = false;
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+//Information about the loading of images and not rendering them/storing 30 frames at a time can be seen in the README file.
+
+var stop = false; //Global variable to stop the animation
+   
+async function animate(extCon, norSouth, dates, monthLoop, starting, ending) {//Main animation function
+    var displayDates = [];//Array for the dates to be displayed
+    $( "#customize :input").prop( "disabled", true );//Disable form entering while animation is running
+    $("#map").html("");//Empty map when a new animation occurs
+    var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];//Days in each month
+    var imageURL = '';//Variable to hold the url to fetch from the server
+    if (norSouth == "n") {//Northern hemisphere values, including the size of the map and the information for the server request
+        var extent = [0,0,304,448];//Size of map
+        var locationVal = "-3850000.0,-5350000.0,3750000.0,5850000.0&width=304&height=448&srs=EPSG:3411";//Location data for request url
     }
-    
-    async function animate(extCon, norSouth, dates, monthLoop, starting, ending) {
-        var displayDates = [];
-        $( "#customize :input").prop( "disabled", true );
-        $("#map").html("");
-        var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-        var imageURL = '';
-        if (norSouth == "n") {
-            var extent = [0,0,304,448];
-            var locationVal = "-3850000.0,-5350000.0,3750000.0,5850000.0&width=304&height=448&srs=EPSG:3411";
-        }
-        else {
-            var extent = [0,0,730,768];
-            var locationVal = "-3950000.0,-3950000.0,3950000.0,4350000.0&width=730&height=768&srs=EPSG:3412";
-        }
-        var projection = new ol.proj.Projection({
-            code: 'local_image',
-            units: 'pixels',
-            extent: extent
-        });
-        
-        var map = new ol.Map({
-            target: 'map',
-            view: new ol.View({
-                projection: projection,
-                center: ol.extent.getCenter(extent),
-                zoom: 1.6,
-            }),
-            controls: []
-        });
-        imageURL = "blank.jpeg";
-        map.addLayer(new ol.layer.Image({
-            source: new ol.source.ImageStatic({
-                url: imageURL,
-                projection: projection,
-                imageExtent: extent
-            })
-        }));
-        displayDates.push("placeholder");
-        for (year = starting[0]; year <= ending[0]; year++) {
-            var monthStart = 1;
-            var monthEnd = 12;
-            if (year == starting[0]) {
-                monthStart = starting[1];
-            }
-            if (year == ending[0]) {
-                monthEnd = ending[1];
-            }
-            if (dates == "SameMonth") {
-                monthStart = monthLoop;
-                monthEnd = monthLoop;
-            }
-            for (month = monthStart; month <= monthEnd; month++) {
-                var dayStart = 1;
-                var dayEnd = monthDays[month-1];
-                if (month == starting[1] && year == starting[0]) {
-                    dayStart = starting[2];
-                }
-                if (month == ending[1] && year == ending[0]) {
-                    dayEnd = ending[2];
-                }
-                if (dates == "Monthly" || dates == "SameMonth") {
-                   dayEnd = 1;
-                   dayStart = 1;
-                }
-                for (day = dayStart; day <= dayEnd; day++) {
-                    if (stop) return;
-                    var monthStr = ""+month;  
-                    if (month < 10) {
-                        monthStr = "0"+month;
-                    }
-                    var dayStr = ""+day;  
-                    if (day < 10) {
-                        dayStr = "0"+day;
-                    }
-                    var yearStr = ""+year;
-                    if (map.getLayers().getArray().length < 30) {
-                        imageURL = "https://nsidc.org/api/mapservices/NSIDC/wms?service=WMS&version=1.1.0&request=GetMap&layers=NSIDC:g02135_"+extCon+"_raster_daily_"+norSouth+"&styles=NSIDC:g02135_"+extCon+"_raster_basemap&bbox="+locationVal+"&format=image/png&TIME="+yearStr+"-"+monthStr+"-"+dayStr;
-                        await map.addLayer(new ol.layer.Image({
-                            source: new ol.source.ImageStatic({
-                                url: imageURL,
-                                projection: projection,
-                                imageExtent: extent
-                            })
-                        }));
-                        await displayDates.push("Date: "+yearStr+"-"+monthStr+"-"+dayStr);
-                    }
-                    else {
-                        
-                        var displayDate = getDisplayDate(year, month, day);
-                        var monthStrDisp = ""+displayDate[1];  
-                        if (displayDate[1] < 10) {
-                            monthStrDisp = "0"+displayDate[1];
-                        }
-                        var dayStrDisp = ""+displayDate[2];  
-                        if (displayDate[2] < 10) {
-                            dayStrDisp = "0"+displayDate[2];
-                        }
-                        var yearStrDisp = ""+displayDate[0];
-                        await displayDates.push("Date: "+yearStr+"-"+monthStr+"-"+dayStr);
-                        
-                        imageURL = "https://nsidc.org/api/mapservices/NSIDC/wms?service=WMS&version=1.1.0&request=GetMap&layers=NSIDC:g02135_"+extCon+"_raster_daily_"+norSouth+"&styles=NSIDC:g02135_"+extCon+"_raster_basemap&bbox="+locationVal+"&format=image/png&TIME="+yearStr+"-"+monthStr+"-"+dayStr;
-                        await map.addLayer(new ol.layer.Image({
-                            source: new ol.source.ImageStatic({
-                                url: imageURL,
-                                projection: projection,
-                                imageExtent: extent
-                            })
-                        }));
-                        map.removeLayer(map.getLayers().getArray()[0]);
-                        map.getLayers().getArray()[0].setZIndex(100);
-                        displayDates.splice(0,1);
-                        $("#date").html(displayDates[0]);
-                        await sleep(2000-Number($("input[name=speed]").val()));
-                    }
-                }
-            }
-        }
-        while (map.getLayers().getArray().length>1){
-            displayDates.splice(0,1);
-            $("#date").html(displayDates[0]);
-            map.removeLayer(map.getLayers().getArray()[0]);
-            map.getLayers().getArray()[0].setZIndex(100);
-            await sleep(2000-Number($("input[name=speed]").val()));
-        }
-        $( "#customize :input").prop( "disabled", false );
+    else {//Information for southern hemisphere
+        var extent = [0,0,730,768];//Map size
+        var locationVal = "-3950000.0,-3950000.0,3950000.0,4350000.0&width=730&height=768&srs=EPSG:3412";//Location for request url
     }
-    
-    
-    
-    function sleep(ms) {
-        return new Promise(resolve => setTimeout(resolve, ms));
-    }
-    
-    function getDisplayDate(y,m,d) {
-        var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-        if (d <= 20) {
-            if (m <= 1) {
-                return [y-1, 12-(m-1),monthDays[12-(m-1)-1]+d-20];
-            }
-            return [y, m-1, monthDays[m-2]+d-20];
-        }
-        return [y,m,d-20];
-    } 
-    
-    function addDay(y,m,d) {
-        var monthDays = [31,28,31,30,31,30,31,31,30,31,30,31];
-        if (d >= monthDays[m-1]){
-            if (m >= 12) {
-                return [y+1, 1, 1];
-            }
-            return [y, m+1, 1];
-        }
-        return [y,m,d+1];
-    }
-    $("document").ready(async function() {
-        $("#animate").click( function() {
-            stop = false;
-            var extCon = $('input[name=ext-con]:checked').val();
-            var norSouth = $('input[name=n-s]:checked').val();
-            var dates = $('input[name=dates]:checked').val();
-            var monthLoop = $('select[name=monthsLoop]').val();
-            var starting = [Number($('input[name=sYear]').val()), Number($('select[name=sMonth]').val()), Number($('input[name=sDay]').val())];
-            var ending = [Number($('input[name=eYear]').val()), Number($('select[name=eMonth]').val()), Number($('input[name=eDay]').val())]
-            animate(extCon,norSouth,dates,monthLoop,starting,ending);
-        });
-        $("#stopAnimation").click( function() {
-            stop = true;
-            $( "#customize :input").prop( "disabled", false );
-        });
+    var projection = new ol.proj.Projection({//Map projection
+        code: 'local_image',
+        units: 'pixels',
+        extent: extent
     });
     
+    var map = new ol.Map({//New map
+        target: 'map',//Div in which the map is displayed
+        view: new ol.View({
+            projection: projection,
+            center: ol.extent.getCenter(extent),//Start in the center of the image
+            zoom: 1.6,//Good start for zoom
+        }),
+        controls: []//Empty controls for now
+    });
+    imageURL = "blank.jpeg";//Placeholder image to add the first frame, as there needs to be one frame that is deleted before any displays happen
+    map.addLayer(new ol.layer.Image({
+        source: new ol.source.ImageStatic({
+            url: imageURL,
+            projection: projection,
+            imageExtent: extent
+        })
+    }));
+    displayDates.push("placeholder");//Placeholder in the dates array, also gets deleted by the program for the first run
+    for (year = starting[0]; year <= ending[0]; year++) {//loop through all years
+        var monthStart = 1;//default starting month
+        var monthEnd = 12;//default ending month
+        if (year == starting[0]) {//If on the starting year, start on the specified month. Otherwise it just starts at 1 (January)
+            monthStart = starting[1];
+        }
+        if (year == ending[0]) {//If on the last year, end on the specified month. Otherwise it ends on 12 (December)
+            monthEnd = ending[1];
+        }
+        if (dates == "SameMonth") {//If it is looping the same month, just loop through that one month
+            monthStart = monthLoop;
+            monthEnd = monthLoop;
+        }
+        for (month = monthStart; month <= monthEnd; month++) {//Loop months
+            var dayStart = 1;//default starting day
+            var dayEnd = monthDays[month-1];//default ending day, month -1 because index starts at 0
+            if (month == starting[1] && year == starting[0]) {//if on first month and year, start at the specified date
+                dayStart = starting[2];
+            }
+            if (month == ending[1] && year == ending[0]) {//if on the last month and year, end at the last date
+                dayEnd = ending[2];
+            }
+            if (dates == "Monthly" || dates == "SameMonth") {//If it is looping only months or the same month, just use the first day of the month
+                dayEnd = 1;
+                dayStart = 1;
+            }
+            for (day = dayStart; day <= dayEnd; day++) {//loop days
+                if (stop) return;//Catch to stop animation
+                var monthStr = ""+month;//create the string for month, ensuring it is 2 digits
+                if (month < 10) {
+                    monthStr = "0"+month;
+                }
+                var dayStr = ""+day;//string for the day, ensuring 2 digits
+                if (day < 10) {
+                    dayStr = "0"+day;
+                }
+                var yearStr = ""+year;//String for year
+                await displayDates.push("Date: "+yearStr+"-"+monthStr+"-"+dayStr);
+                    imageURL = "https://nsidc.org/api/mapservices/NSIDC/wms?service=WMS&version=1.1.0&request=GetMap&layers=NSIDC:g02135_"+extCon+"_raster_daily_"+norSouth+"&styles=NSIDC:g02135_"+extCon+"_raster_basemap&bbox="+locationVal+"&format=image/png&TIME="+yearStr+"-"+monthStr+"-"+dayStr;
+                    await map.addLayer(new ol.layer.Image({
+                        source: new ol.source.ImageStatic({
+                            url: imageURL,
+                            projection: projection,
+                            imageExtent: extent
+                        })
+                    }));
+                if (!(map.getLayers().getArray().length < 30)) {//If there are 30 layers already loaded, display them. Otherwise, just load the layers but do not display.
+                    map.removeLayer(map.getLayers().getArray()[0]);//remove the current top layer
+                    map.getLayers().getArray()[0].setZIndex(100);//set the next layer to be on top
+                    displayDates.splice(0,1);//remove the current date value
+                    $("#date").html(displayDates[0]);//set the date to be the next day, what is displayed by the layer
+                    await sleep(2000-Number($("input[name=speed]").val()));//sleep for the time specified by the slider bar
+                
+                }
+            }
+        }
+    }
+    while (map.getLayers().getArray().length>1){//While there are unrendered layers remaining, loop through them
+        displayDates.splice(0,1);//remove the date
+        $("#date").html(displayDates[0]);//update the date to current
+        map.removeLayer(map.getLayers().getArray()[0]);//remove current layer
+        map.getLayers().getArray()[0].setZIndex(100);//add the new layer
+        await sleep(2000-Number($("input[name=speed]").val()));//sleep for the correct time
+    }
+    $( "#customize :input").prop( "disabled", false );//reenable the form when everything is done
+}
+
+$("document").ready(async function() {//When DOM is loaded
+    $("#animate").click( function() {//When animation button is clicked
+        stop = false;//Don't stop animation
+        var extCon = $('input[name=ext-con]:checked').val();//Get value for extent or concentration
+        var norSouth = $('input[name=n-s]:checked').val();//Get value for North or South
+        var dates = $('input[name=dates]:checked').val();//Get value for the looping style
+        var monthLoop = $('select[name=monthsLoop]').val();//Month to be repeated if that option is chosen
+        var starting = [Number($('input[name=sYear]').val()), Number($('select[name=sMonth]').val()), Number($('input[name=sDay]').val())];//year, month, day starting values
+        var ending = [Number($('input[name=eYear]').val()), Number($('select[name=eMonth]').val()), Number($('input[name=eDay]').val())]//year, month, day ending values
+        animate(extCon,norSouth,dates,monthLoop,starting,ending);//animate
+    });
+    $("#stopAnimation").click( function() {//Stop button
+        stop = true;//stop the animation
+        $( "#customize :input").prop( "disabled", false );//Reenable the form to allow the animation to be restarted
+    });
+});
+
+function sleep(ms) { //Sleep function for pauses between frames
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
