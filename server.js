4bf01c70538b1da53072b756b865d57824366c6f
SamHaggans <sam@samhaggans.com>
2020-03-26 12:26:32 -0600

    Add eslint and use google style guide to reformat code

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-var express = require("express");
+var express = require('express');
 var server = express();
 var port = process.env.PORT || 3003;
 server.use(express.static('public'));
 
-var server = server.listen(port, function(){
-    var port = server.address().port;
-    console.log("Server started on port: localhost:%s", port);
+var server = server.listen(port, function() {
+  const port = server.address().port;
+  console.log('Server started on port: localhost:%s', port);
 });
