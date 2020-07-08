f89c258ccd3b65fc82dd28a4dcc5334f43a6e6cf
SamHaggans <samuelhaggans@gmail.com>
2020-07-08 12:34:39 -0600

    Finish removing babel

--- webpack.config.js
+++ webpack.config.js
@@ -15,9 +15,6 @@ module.exports = {
     rules: [{
       test: /\.(js|jsx)$/,
       exclude: /node_modules/,
-      use: {
-        loader: 'babel-loader',
-      },
     },
     {
       test: /\.css$/,
