c041bca6a5b80956bc6a4d032087fac5cd134f24
SamHaggans <samuelhaggans@gmail.com>
2020-07-29 12:38:25 -0600

    Add footer with version number

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const pkg = require('./package.json');
 
 const outputDirectory = path.join(__dirname, 'dist');
 
@@ -38,6 +39,7 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       template: './public/index.ejs',
+      version: pkg.version,
     }),
   ],
 };
