1741b9eeef75c8950f95b322f0c5423bdb624e50
SamHaggans <samuelhaggans@gmail.com>
2020-08-12 10:58:30 -0600

    Add changes for webpack mode configuration

--- webpack.config.js
+++ webpack.config.js
@@ -5,41 +5,52 @@ const pkg = require('./package.json');
 
 const outputDirectory = path.join(__dirname, 'dist');
 
-module.exports = {
-  devtool: 'inline-source-map',
-  entry: './public/script.js',
-  output: {
-    path: outputDirectory,
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [{
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
+module.exports = (env) =>{
+  let mode = 'development';
+
+  if (env && env.hasOwnProperty('NODE_ENV') && env.NODE_ENV === 'production') {
+    mode = 'production';
+  }
+
+  console.log('Using', mode, 'settings.');
+
+  return {
+    mode: mode,
+    devtool: 'inline-source-map',
+    entry: './public/script.js',
+    output: {
+      path: outputDirectory,
+      filename: 'bundle.js',
     },
-    {
-      test: /\.css$/,
-      use: ['style-loader', 'css-loader'],
+    module: {
+      rules: [{
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        loader: 'file-loader',
+      }],
     },
-    {
-      test: /\.(jpe?g|png|gif|svg)$/,
-      loader: 'file-loader',
-    }],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-  devServer: {
-    port: 3030,
-    contentBase: outputDirectory,
-  },
-  plugins: [
-    new CleanWebpackPlugin({
-      cleanStaleWebpackAssets: false,
-    }),
-    new HtmlWebpackPlugin({
-      template: './public/index.ejs',
-      version: pkg.version,
-    }),
-  ],
+    resolve: {
+      extensions: ['*', '.js', '.jsx'],
+    },
+    devServer: {
+      port: 3030,
+      contentBase: outputDirectory,
+    },
+    plugins: [
+      new CleanWebpackPlugin({
+        cleanStaleWebpackAssets: false,
+      }),
+      new HtmlWebpackPlugin({
+        template: './public/index.ejs',
+        version: pkg.version,
+      }),
+    ],
+  }
 };
